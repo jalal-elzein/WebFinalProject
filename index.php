f93498297949f5f33091146effec21e8c10de9dc
RayYas03 <rayane_yassine03@hotmail.com>
2023-04-24 18:05:50 +0300

    login page

---
+++ index.php
@@ -0,0 +1,58 @@
+
+
+<html>
+    <head>  
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <!-- <link rel="stylesheet" href="../Styles/homiecss.css"> -->
+    <link rel="stylesheet" href="Styles/loginstyles.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
+        <title>
+            Login
+        </title>
+    </head>
+    <body>
+                    
+
+        <div class="title">
+            <span>Log Into Your Account</h1>
+        </div>        
+        
+    
+        <div class="paragraph" style="border:0;">
+            <form class="form" action="./backend/login.php" method="POST" name="login-form">
+                <label for="username">Username</label>
+                <br>
+                <input type="text" name="username" id="uninput" class="txtfield">
+                <br><br>
+                <label for="password">Password</label>
+                <br>
+                <input type="password" name="password" id="pinput" class="txtfield">
+                <br><br>
+                <button onclick="window.location.href='..';" class="btn btn-outline-danger" type="button">
+                  <i class="fas fa-user"></i>
+                  Login
+                </button>
+                
+                
+            </form>
+
+            
+
+            <img src="./Images/mar3.png" class="jump" id="mario-homepage" height="300px" style="float: right;">
+        </div>
+
+        <span id="login" style=" -webkit-text-stroke: 0;margin-right: 10; font-family: sans-serif; position:relative; top:220; left:42%; color: white;">
+            Don't have an account? Sign up!
+            <br>
+        </span>
+        <br>
+        <button onclick="window.location.href='..';" class="btn btn-outline-danger" type="button" style="top: 200;left: 46.5%;position: relative;">
+                <i class="fas fa-user"></i>
+                Sign Up
+        </button>
+
+        
+                
+            
+
+        
