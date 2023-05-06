b4e059211f5de72cb5e331fd59af369860b27409
Admin <jalal13elzein@gmail.com>
2023-05-06 16:38:50 +0300

    styled the trivia page and its leaderboad page

--- Pages/leaderboards/trivia.php
+++ Pages/leaderboards/trivia.php
@@ -7,42 +7,57 @@
         <title>Trivia Leaderboard</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
             integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+        <link rel="stylesheet" href="../../Styles/trivia_lb.css">
     </head>
 
     <body>
-        <div class="container text-center">
-            <h1>Trivia Leaderboard</h1>
-            <div class="card">
-                <table class="table table-striped table-hover">
-                    <thead>
-                        <tr>
-                            <th scope="col">#</th>
-                            <th scope="col">Username</th>
-                            <th scope="col">Score</th>
-                            <th scope="col">Date</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php
-                        require_once("../../Backend/database/db_functions.php");
-                        $db = connectToDB();
-                        $qry = "SELECT * FROM trivia_scores ORDER BY score DESC LIMIT 10;";
-                        $qry_res = $db->query($qry);
-                        for ($i = 1; $i <= 10; $i++) {
-                            $row = $qry_res->fetch();
-                            $username = $db->query("SELECT username FROM users WHERE id = " . $row['user_id'] . ";")->fetch()["username"];
-                            $score = "<tr>
-                                <th scope='row'>$i</th>
-                                <td>" . $username . "</td>
-                                <td>" . $row["score"] . "</td>
-                                <td>" . $row["date"] . "</td>
-                                </tr>";
-                            echo $score;
-                        }
-                        ?>
-                    </tbody>
-                </table>
+        <div class="container text-center align-items-center" style="height: 100vh;">
+            <br>
+        
+            <div class="row">
+                <h1>Trivia Leaderboard</h1>
+            </div>    
+
+            <div class="row">
+                <div class="card glass backg-blurred">
+                    <table class="table table-striped table-hover">
+                        <thead>
+                            <tr class="text-white">
+                                <th scope="col">#</th>
+                                <th scope="col">Username</th>
+                                <th scope="col">Score</th>
+                                <th scope="col">Date</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php
+                            require_once("../../Backend/database/db_functions.php");
+                            $db = connectToDB();
+                            $qry = "SELECT * FROM trivia_scores ORDER BY score DESC LIMIT 10;";
+                            $qry_res = $db->query($qry);
+                            for ($i = 1; $i <= 10; $i++) {
+                                $row = $qry_res->fetch();
+                                $username = $db->query("SELECT username FROM users WHERE id = " . $row['user_id'] . ";")->fetch()["username"];
+                                $score = "<tr class='text-white'>
+                                    <th scope='row'>$i</th>
+                                    <td>" . $username . "</td>
+                                    <td>" . $row["score"] . "</td>
+                                    <td>" . $row["date"] . "</td>
+                                    </tr>";
+                                echo $score;
+                            }
+                            ?>
+                        </tbody>
+                    </table>
+
+                </div>
+                <div class="text-center">
+                    <button class="btn bol-inverse w-50 answer-btn" onclick="window.location.href = '../trivia.html';">Play The Game</button>
+                    <button class="btn bol-inverse w-50 hidden" id="home"
+                    onclick="window.location.href = '../index.html';">Home</button>
+                </div>
             </div>
+
         </div>
         
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
