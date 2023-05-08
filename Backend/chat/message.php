6b48e98d5c0879c43da68ce67c544182e3a1f43a
Admin <jalal13elzein@gmail.com>
2023-05-08 14:02:57 +0300

    made the chat better but the background ugly

--- Backend/chat/message.php
+++ Backend/chat/message.php
@@ -1,7 +1,9 @@
 <?php
 
+
 class message {
     private $id, $user_id, $content, $time;
+    public $colors = array("red", "blue", "green", "orange", "purple", "hotpink", "yellow");
 
     public function getID () {
         return $this->id;
@@ -49,18 +51,20 @@ class message {
             $user_qry = "SELECT * FROM users WHERE id = ".$res2["user_id"].";";
             $thing = $db->query($user_qry);
             $res3 = $thing->fetch();
+            $color_i = $res2["user_id"] % 7;
             ?>
-            <!-- <span class="usernameS"><?php echo $res3["username"]; ?></span>
-            <br>
-            <span class="messageS"><?php echo $res2["content"]; ?></span>
-            <br><br> -->
-
-            <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title"><?php echo $res3["username"]; ?></h5>
-                        <p class="card-text"><?php echo $res2["content"]; ?></p>
-                    </div>
+            <!-- TODO: change this to session username -->
+            <div class="card <?php
+                                if ($res3["username"] == "jalal-elzein") echo "my-message"; 
+                                else echo "other-message"; 
+                            ?> "
+            >
+                <div class="card-body">
+                    <h5 class="card-title" style="color: <?php echo $this->colors[$color_i]; ?>"><?php echo $res3["username"]; ?></h5>
+                    <p class="card-text"><?php echo $res2["content"]; ?></p>
+                    <h6 class="card-subtitle mb-2" style="color: <?php echo $this->colors[$color_i]; ?>; opacity: 0.5; float: right;"><?php echo $res2["time"]; ?></h6>
                 </div>
+            </div>
             <?php
         }
     }
