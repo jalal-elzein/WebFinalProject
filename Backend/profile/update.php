c170ef8c225099e3b402e047d8d6dc3c4e7d9247
Admin <jalal13elzein@gmail.com>
2023-05-10 13:19:10 +0300

    half-fixed signup

--- Backend/profile/update.php
+++ Backend/profile/update.php
@@ -21,12 +21,6 @@ $email = $new_info_array['email'];
 $phone = $new_info_array['phone'];
 $birthdate = $new_info_array['birthdate'];
 
-// $first_name = $_POST["first_name"];
-// $last_name = $_POST["last_name"];
-// $email = $_POST["email"];
-// $phone = $_POST["phone"];
-// $birthdate = $_POST["birthdate"];
-
 $qry = "UPDATE users SET first_name='$first_name', last_name='$last_name', email='$email', phone='$phone', birthdate='$birthdate' WHERE id=$user_id;";
 
 try {
