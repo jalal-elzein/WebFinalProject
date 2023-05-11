5486aee84e9285355219148e531590922e397929
RayYas03 <rayane_yassine03@hotmail.com>
2023-05-12 00:43:34 +0300

    comments

--- Backend/profile/get.php
+++ Backend/profile/get.php
@@ -4,8 +4,10 @@ session_start();
 
 require_once("../database/db_functions.php");
 
+//connect to db
 $db = connectToDB();
 
+//retrieve user info
 $user_id = $_SESSION["user_id"];
 $username = $_SESSION["username"];
 
@@ -13,6 +15,7 @@ $qry = "SELECT * FROM users WHERE id = $user_id";
 $qry1 = $db->query($qry);
 $qry2 = $qry1->fetch();
 
+//store variables
 $email = $qry2["email"];
 $first_name = $qry2["first_name"];
 $last_name = $qry2["last_name"];
@@ -22,8 +25,10 @@ $twitter_link = $qry2["twitter_link"];
 $instagram_link = $qry2["instagram_link"];
 $facebook_link = $qry2["facebook_link"];
 
+//store user info in an array
 $info = array($username, $email, $first_name, $last_name, $phone, $birthdate, $twitter_link, $instagram_link, $facebook_link);
 
+//encode the $info array as a JSON string
 echo json_encode($info);
 
 ?>
